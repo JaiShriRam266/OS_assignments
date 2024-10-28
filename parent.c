e0e9c2a99bd13e803f73f6b4aadc341219a2bc90
JaiShriRam266 <avdhutadhav530@outlook.com>
2024-10-28 19:41:23 +0530

    Add files via upload

---
+++ parent.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main() {
+int n;
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+char *args[n + 2];
+args[0] = "./2Bchild"; // Program to execute
+args[n + 1] = NULL; // Null terminate the arguments
+printf("Enter the elements of the array:\n");
+for (int i = 1; i <= n; i++) {
+args[i] = (char *)malloc(10 * sizeof(char));
+scanf("%s", args[i]);
+}
+pid_t pid = fork();
+if (pid < 0) {
+perror("Fork failed");
+exit(EXIT_FAILURE);
+}
+if (pid == 0) { // Child process
+execve(args[0], args, NULL);
+perror("Execve failed");
+exit(EXIT_FAILURE);
+} else { // Parent process
+wait(NULL);
+printf("Parent process: Child process has completed.\n");
+for (int i = 1; i <= n; i++) {
+free(args[i]);
+} }
+return 0; }
